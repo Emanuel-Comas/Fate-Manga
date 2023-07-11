1ca5cb78d9d25180a2964eb4c4af8c8a88c257c8
Emanuel comas <Emanuelcomas96@gmail.com>
2023-07-11 19:38:25 -0300

    Agregado parcial de html y css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="Hero">
+        <h1>TuMangaOnline</h1>
+      </div>
+
+      <div className="Manga1">
+              <p className="Titulo-manga1">Fate/Stay Night: Heaven feel (2015)</p>
+            <img src="https://otakuteca.com/images/books/cover/6385349ea005b.jpg" alt="Manga1" />
+            <button className="Btn">Info</button>
+      </div>
+
+      <div className="Manga2">
+              <p className="Titulo-manga2">FGO. Mangade Wakaru! (2015)</p>
+            <img src="https://otakuteca.com/images/books/cover/30396_TMOManga59a7ad0ca3603.jpg" alt="Manga2" />
+            <button className="Btn">Info</button>
+      </div>
+
+      <div className="Manga3">
+              <p className="Titulo-manga3">Fate/Stay Night: Master Arturia y Servant Shirou (2019-2019) </p>
+            <img src="https://otakuteca.com/images/books/cover/5c31810f446ae.jpg" alt="Manga3" />
+            <button className="Btn">Info</button>
+      </div>
+
+      <div className="Manga4">
+              <p className="Titulo-manga4">Fate/Grand Order: -Epic of Remnant- Deep Sea Cyber-Paradise SE.RA.PH (2019)</p>
+            <img src="https://otakuteca.com/images/books/cover/62fdb983713e0.jpg" alt="Manga4" />
+            <button className="Btn">Info</button>
+      </div>
+
+      <div className="Manga5">
+              <p className="Titulo-manga5">Fate/Type Redline (2019)</p>
+            <img src="https://otakuteca.com/images/books/cover/5e00e3c147517.jpg" alt="Manga5" />
+            <button className="Btn">Info</button>
+      </div>
+
+      <div className="Manga6">
+              <p className="Titulo-manga6">Fate/kaleid liner PRISMA☆ILLYA 3rei!! (2012)</p>
+            <img src="https://otakuteca.com/images/books/cover/63993b5cd7ae5.jpg" alt="Manga6" />
+            <button className="Btn">Info</button>
+      </div>
+
     </div>
   );
 }
